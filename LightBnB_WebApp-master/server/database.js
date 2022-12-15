9da26d950e63776d620ef7edb195e16fd7eeedae
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-12-15 11:32:58 -0800

    add dotenv.create .env file in gitignore. update getallproperties function.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,19 @@
+require('dotenv').config();
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const config = {
+  user: process.env.lightbnbUser,
+  password: process.env.lightbnbPassword,
+  host: process.env.lightbnbHost,
+  database: process.env.lightbnbDatabase
+};
+
+const pool = new Pool (config);
+pool.connect();
+
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log('database connected')});
 
 /// Users
 
@@ -66,13 +80,17 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+ const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllProperties = getAllProperties;
 
 
